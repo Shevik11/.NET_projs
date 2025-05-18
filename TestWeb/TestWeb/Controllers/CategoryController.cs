42630bea35f81054cadf0c9e2d5cce1fa806aa75
Shevik11 <maksshevchuk621@gmail.com>
2025-05-18 19:16:32 +0300

    start project

---
+++ TestWeb/TestWeb/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestWeb.Data;
+using TestWeb.Models;
+
+namespace TestWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> ObjCategoryList = _db.Categories.ToList();
+            return View(ObjCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TestWeb/TestWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestWeb.Models;
+
+namespace TestWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category
+                {
+                    Id = 1,
+                    Name = "Appetizers",
+                    DisplayedOrder = 1
+                },
+                new Category
+                {
+                    Id = 2,
+                    Name = "Main Course",
+                    DisplayedOrder = 2
+                },
+                new Category
+                {
+                    Id = 3,
+                    Name = "Desserts",
+                    DisplayedOrder = 3
+                }
+                );
+        }
+    }
+}
